e006aaa0c0141a72e5edaefaa972d1963212532d
tonandton <www.sorasakok@gmail.com>
2022-07-22 23:20:05 +0700

    create gird list and gird view

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { FaCheck } from "react-icons/fa";
@@ -9,6 +9,27 @@ const AddToCart = ({ product }) => {
     const { id, stock, colors } = product;
 
     const [mainColor, setMainColor] = useState(colors[0]);
+    const [amount, setAmount] = useState(1);
+
+    const increase = () => {
+        setAmount((oldAmount) => {
+            let tempAmount = oldAmount + 1;
+            if (tempAmount > stock) {
+                tempAmount = stock;
+            }
+            return tempAmount;
+        });
+    };
+
+    const decrease = () => {
+        setAmount((oldAmount) => {
+            let tempAmount = oldAmount - 1;
+            if (tempAmount < 1) {
+                tempAmount = 1;
+            }
+            return tempAmount;
+        });
+    };
 
     return (
         <Wrapper>
@@ -33,7 +54,16 @@ const AddToCart = ({ product }) => {
                     })}
                 </div>
             </div>
-            <div className="btn-container"></div>
+            <div className="btn-container">
+                <AmountButtons
+                    amount={amount}
+                    increase={increase}
+                    decrease={decrease}
+                />
+                <Link to="/cart" className="btn">
+                    Add to Cart
+                </Link>
+            </div>
         </Wrapper>
     );
 };
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -1,17 +1,38 @@
 import {
-  LOAD_PRODUCTS,
-  SET_LISTVIEW,
-  SET_GRIDVIEW,
-  UPDATE_SORT,
-  SORT_PRODUCTS,
-  UPDATE_FILTERS,
-  FILTER_PRODUCTS,
-  CLEAR_FILTERS,
-} from '../actions'
+    LOAD_PRODUCTS,
+    SET_LISTVIEW,
+    SET_GRIDVIEW,
+    UPDATE_SORT,
+    SORT_PRODUCTS,
+    UPDATE_FILTERS,
+    FILTER_PRODUCTS,
+    CLEAR_FILTERS,
+} from "../actions";
 
 const filter_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+    if (action.type === LOAD_PRODUCTS) {
+        return {
+            ...state,
+            all_products: [...action.payload],
+            filtered_products: [...action.payload],
+        };
+    }
 
-export default filter_reducer
+    if (action.type === SET_GRIDVIEW) {
+        return {
+            ...state,
+            grid_view: true,
+        };
+    }
+
+    if (action.type === SET_LISTVIEW) {
+        return {
+            ...state,
+            grid_view: false,
+        };
+    }
+
+    throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default filter_reducer;
